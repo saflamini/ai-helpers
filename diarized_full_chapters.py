dc076413158061760ebdf930afc3c9b06fbac795
saflamini <43142465+saflamini@users.noreply.github.com>
2023-10-18 21:48:05 -0700

    Update diarized_full_chapters.py

--- diarized_full_chapters.py
+++ diarized_full_chapters.py
@@ -1,5 +1,7 @@
 from output import json_output
 
+#Give this function a json response that has auto_chapters and speaker_labels set to true
+#see the output formatted in a way that makes it easy to embed & store each individual chapter in a vector DB
 def speaker_diarized_output(json_response):
     # Check if auto_chapters and speaker_labels are true
     if not (json_response.get('auto_chapters') and json_response.get('speaker_labels')):
